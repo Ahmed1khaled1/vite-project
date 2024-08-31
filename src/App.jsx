db4e8d4db005620842c2eda0015d0cdf92688e68
Ahmed-khaled <158309985+Ahmed1khaled1@users.noreply.github.com>
2024-08-31 20:06:12 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -34,7 +34,7 @@ const[darkMood,setDarkMood]=useState(false);
           </ul>
         </nav>
         <div className='text-center p-10'>
-          <h2 className='text-5xl py-2 text-teal-500 font md:text-6xl'>Ahmed Khaled</h2>
+          <h2 className='text-4xl py-2 text-teal-500 font md:text-6xl'>Ahmed Khaled</h2>
           <h3 className='text-2xl font-medium py-2 md:text-3xl  dark:text-white'>
             Front-end Developer</h3>
           <p className='leading-8 text-gray-800 dark:text-white'>
